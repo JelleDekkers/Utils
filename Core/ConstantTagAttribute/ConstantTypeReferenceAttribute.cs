377e81c3c1f1976992822178336d3f5f81eb07a7
JelleDekkers <jelle.dekkers@hotmail.com>
2019-12-12 15:42:11 +0100

    Added unfinished constant type reference attribute

---
+++ Core/ConstantTagAttribute/ConstantTypeReferenceAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utils.Core
+{
+	/// <summary>
+	/// Attribute for showing constant fields of a certain type in the editor
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+	public class ConstantTypeReferenceAttribute : PropertyAttribute
+	{
+		public readonly Type constType;
+
+		public ConstantTypeReferenceAttribute(Type type, Type constType)
+		{
+			this.constType = constType;
+		}
+	}
+}
