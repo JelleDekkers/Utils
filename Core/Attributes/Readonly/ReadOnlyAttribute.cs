fd21ebdfc51af8a43b0d0f5b5a7265ce0edd0f4e
JelleDekkers <jelle.dekkers@hotmail.com>
2020-07-16 14:38:26 +0200

    Added readonly attribute

---
+++ Core/Attributes/Readonly/ReadOnlyAttribute.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Utils.Core.Attributes
+{
+    /// <summary>
+    /// Simple attribute for rendering a non interactable, read-only value in the editor
+    /// </summary>
+    public class ReadOnlyAttribute : PropertyAttribute
+    {
+
+    }
+}
