d61ea149b9142aa716283ca224a154ca1a4c0353
JelleDekkers <jelle.dekkers@hotmail.com>
2020-07-31 16:21:00 +0200

    Added dontdestroyonload option on spawnprefab action

--- Core/StateMachine/Data/DefaultActions/SpawnPrefabAction.cs
+++ Core/StateMachine/Data/DefaultActions/SpawnPrefabAction.cs
@@ -6,6 +6,7 @@ namespace Utils.Core.Flow.DefaultActions
     public class SpawnPrefabAction : StateAction
     {
         [SerializeField] protected GameObject prefab = null;
+        [SerializeField] protected bool dontDestroyOnLoad = false;
 
         protected GameObject instance;
         protected DependencyInjector injector;
@@ -23,6 +24,9 @@ namespace Utils.Core.Flow.DefaultActions
         protected virtual void SpawnPrefab()
         {
             instance = injector.InstantiateGameObject(prefab);
+
+            if(dontDestroyOnLoad)
+                DontDestroyOnLoad(instance);
         }
     }
 }
