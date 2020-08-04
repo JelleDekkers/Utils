92472e0db8dd229b465e8c48f00f07b487b2f09b
JelleDekkers <jelle.dekkers@hotmail.com>
2020-08-04 11:58:58 +0200

    Implemented injectedObjectSpawner

---
+++ Core/DependencyInjection/InjectedObjectSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Utils.Core.Events;
+
+namespace Utils.Core.Injection
+{
+    public class InjectedObjectSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject prefab = null;
+
+        protected virtual void Awake()
+        {
+            Spawnprefab();
+        }
+
+        protected virtual void Spawnprefab()
+        {
+            DependencyInjector injector = new DependencyInjector(prefab.name);
+            EventDispatcher eventDispatcher = new EventDispatcher(prefab.name);
+            injector.RegisterInstance<EventDispatcher>(eventDispatcher);
+
+            GameObject instance = injector.InstantiateGameObject(prefab);
+            instance.transform.position = transform.position;
+            instance.transform.rotation = transform.rotation;
+        }
+    }
+}
