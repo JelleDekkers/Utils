09e12f4245a42119c4789239f96252f94b4f2ddf
JelleDekkers <jelle.dekkers@hotmail.com>
2020-12-01 13:46:59 +0100

    Added layerMaskExtensions

---
+++ Core/Extensions/LayerMaskExtensions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Utils.Core.Extensions
+{
+    public static class LayerMaskExtensions 
+    {
+        public static bool LayerMaskContainsLayer(LayerMask layerMask, GameObject obj)
+        {
+            return ((layerMask.value & (1 << obj.layer)) > 0);
+        }
+
+        public static bool LayerMaskContainsLayer(LayerMask layerMask, int layer)
+        {
+            return ((layerMask.value & (1 << layer)) > 0);
+        }
+
+        public static bool ContainsLayer(this LayerMask layerMask, int layer)
+        {
+            return LayerMaskContainsLayer(layerMask, layer);
+        }
+    }
+}
