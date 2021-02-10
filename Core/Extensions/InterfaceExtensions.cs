7717e5332bcd38d25a9a2cef2a2391afd761846e
JelleDekkers <jelle.dekkers@hotmail.com>
2021-02-10 16:25:29 +0100

    Added TryGetInterface extension

--- Core/Extensions/InterfaceExtensions.cs
+++ Core/Extensions/InterfaceExtensions.cs
@@ -14,6 +14,17 @@ namespace Utils.Core.Extensions
             return obj.GetComponent<T>() as T;
         }
 
+        public static bool TryGetInterface<T>(this GameObject obj, out T @interface) where T : class
+        {
+            if (!typeof(T).IsInterface)
+            {
+                throw new System.Exception(typeof(T).ToString() + ": is not an interface!");
+            }
+
+            @interface = obj.GetComponent<T>() as T;
+            return @interface != null;
+        }
+
         public static T[] GetInterfaces<T>(this GameObject obj) where T : class
         {
             if (!typeof(T).IsInterface)
