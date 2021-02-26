607fb3e9dbeaf0df782bf87d150451bad9002eec
JelleDekkers <jelle.dekkers@hotmail.com>
2021-02-26 16:03:56 +0100

    Replaced scenePathAttribute with SceneField which fixes the issue with changing scene name and changing scene folder

---
+++ Core/SceneField/Editor/SceneFieldPropertyDrawer.cs
@@ -0,0 +1,47 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(SceneField))]
+public class SceneFieldPropertyDrawer : PropertyDrawer
+{
+    private const float BackLabelWidth = 100;
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, GUIContent.none, property);
+        SerializedProperty sceneAsset = property.FindPropertyRelative("sceneAsset");
+
+        position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+        position.width -= BackLabelWidth;
+        EditorGUI.BeginChangeCheck();
+        var sceneField = EditorGUI.ObjectField(position, sceneAsset.objectReferenceValue, typeof(SceneAsset), false);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            sceneAsset.objectReferenceValue = sceneField;
+        }
+
+        if (sceneAsset.objectReferenceValue != null)
+        {
+            position.x += position.width;
+            position.width = BackLabelWidth;
+            bool isAddedInBuild = IsSceneAddedToBuild(sceneAsset.objectReferenceValue as SceneAsset);
+            string text = isAddedInBuild ? "In build" : "Not in build";
+            EditorGUI.LabelField(position, new GUIContent(text, "Shows if this scene is added to the build list and will load when switching scenes"), EditorStyles.miniLabel);
+        }
+        EditorGUI.EndProperty();
+    }
+
+    private bool IsSceneAddedToBuild(SceneAsset scene)
+    {
+        for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(EditorBuildSettings.scenes[i].guid.ToString());
+            SceneAsset asset = AssetDatabase.LoadAssetAtPath(path, typeof(SceneAsset)) as SceneAsset;
+
+            if (asset == scene)
+                return true; 
+        }
+        return false;
+    }
+}
---
+++ Core/SceneField/SceneField.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Class for properly displaying a scene in the inspector. Won't lose track of the scene when changing it's name and/or change folders
+/// Shows wether the scene is added to the build list in the inspector
+/// </summary>
+[System.Serializable]
+public class SceneField
+{
+    [SerializeField] private Object sceneAsset = null;
+
+    public string SceneName => sceneAsset.name;
+
+    // makes it work with the existing Unity methods (LoadLevel/LoadScene)
+    public static implicit operator string(SceneField sceneField)
+    {
+        return sceneField.SceneName;
+    }
+}
