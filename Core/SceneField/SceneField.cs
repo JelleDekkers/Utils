956ee6182110c722be81a239ec19b9b4dc667ac7
JelleDekkers <jelle.dekkers@hotmail.com>
2021-05-11 14:53:53 +0200

    Updated sceneField with ctor for Vincent

--- Core/SceneField/SceneField.cs
+++ Core/SceneField/SceneField.cs
@@ -23,12 +23,16 @@ namespace Utils.Core.SceneManagement
 
         public string SceneName => sceneName;
 
+#if UNITY_EDITOR
         public SceneField(Object sceneAsset)
         {
-#if UNITY_EDITOR
             this.sceneAsset = sceneAsset;
-#endif
             sceneName = sceneAsset.name;
+        }
+#endif
+        public SceneField(string name)
+        {
+            sceneName = name;
         }
 
         public static implicit operator string(SceneField sceneField)
