6ec7a61bf2c265fa3c65a8358f7755b5a342e7ef
KHS-Jelle <jelle.dekkers@hotmail.com>
2022-02-10 17:44:05 +0100

    Fixed issue where SceneField would not serialize scene name

--- Core/SceneField/SceneField.cs
+++ Core/SceneField/SceneField.cs
@@ -1,41 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Utils.Core.SceneManagement
 {
-	/// <summary>
-	/// Class for properly displaying a scene in the inspector. Won't lose track of the scene when changing folders
-	/// Conveniently shows wether the scene is added to the build list in the inspector
-	/// 
-	/// Known issue: should the SceneAsset's name change, this variable needs to be focused in order to update the sceneName string
-	/// 
-	/// Keeps track of a sceneAsset and sceneName. When making a build, the sceneAsset's reference gets lost due to how Unity internally handles scene assets, 
-	/// therefore a seperate string is needed
-	/// </summary>
-	[System.Serializable]
+    /// <summary>
+    /// Class for properly displaying a scene in the inspector. Won't lose track of the scene when changing folders
+    /// Conveniently shows wether the scene is added to the build list in the inspector
+    /// 
+    /// Known issue: should the SceneAsset's name change, this variable needs to be focused in order to update the sceneName string
+    /// 
+    /// Keeps track of a sceneAsset and sceneName. When making a build, the sceneAsset's reference gets lost due to how Unity internally handles scene assets, 
+    /// therefore a seperate string is needed
+    /// </summary>
+    [System.Serializable]
 	public class SceneField
 	{
 #if UNITY_EDITOR
 #pragma warning disable CS0414
-		[SerializeField] protected Object sceneAsset = null;
+		[SerializeField] public Object sceneAsset = null;
 #pragma warning disable CS0414
 #endif
-		[SerializeField] protected string sceneName = string.Empty;
-
-		public string SceneName => sceneName;
+		public string SceneName => sceneAsset.name;
 
 #if UNITY_EDITOR
 		public SceneField(Object sceneAsset)
 		{
 			this.sceneAsset = sceneAsset;
-			sceneName = sceneAsset.name;
 		}
 #endif
-		public SceneField(string name)
-		{
-			sceneName = name;
-		}
 
 		public static implicit operator string(SceneField sceneField)
 		{
