21e7726cae374b32af70cfb1e155c7619ebadd2e
KHS-Jelle <jelle.dekkers@hotmail.com>
2022-09-12 17:27:55 +0200

    Placed ScriptBuilder into seperate folder and namespace and added ObjectPool namespace

--- Core/ObjectPool/IPoolable.cs
+++ Core/ObjectPool/IPoolable.cs
@@ -1,40 +1,43 @@
 ﻿using System;
 
-public interface IPoolable
+namespace Utils.Core.ObjectPooling
 {
-    /// <summary>
-    /// Is the object in the pool or is it being used?
-    /// </summary>
-    bool IsBeingUsed { get; }
+    public interface IPoolable
+    {
+        /// <summary>
+        /// Is the object in the pool or is it being used?
+        /// </summary>
+        bool IsBeingUsedByPool { get; }
 
-    /// <summary>
-    /// Called when the object is returned to the pool
-    /// </summary>
-    Action<IPoolable> OnReturnedToPool { get; set; }
+        /// <summary>
+        /// Called when the object is returned to the pool
+        /// </summary>
+        Action<IPoolable> OnReturnedToPool { get; set; }
 
-    /// <summary>
-    /// Reset to initial state after being used from pool
-    /// </summary>
-    void ResetState();
+        /// <summary>
+        /// Reset to initial state after being used from pool
+        /// </summary>
+        void ResetState();
 
-    /// <summary>
-    /// Called when taken out of the pool to be active
-    /// </summary>
-    void BecomeActive();
+        /// <summary>
+        /// Called when taken out of the pool to be active
+        /// </summary>
+        void BecomeActive();
 
-    /// <summary>
-    /// Called when to be put back into pool of inactive objects
-    /// </summary>
-    void BecomeInactive();
+        /// <summary>
+        /// Called when to be put back into pool of inactive objects
+        /// </summary>
+        void BecomeInactive();
 
-    /// <summary>
-    /// Return this object to the pool, making it inactive.
-    /// </summary>
-    void ReturnToPool();
+        /// <summary>
+        /// Return this object to the pool, making it inactive.
+        /// </summary>
+        void ReturnToPool();
 
-    /// <summary>
-    /// Destroy object
-    /// Not called Destroy in case of MonoBehaviour 
-    /// </summary>
-    void DestroyObject();
+        /// <summary>
+        /// Destroy object
+        /// Not called Destroy in case of MonoBehaviour 
+        /// </summary>
+        void DestroyObject();
+    }
 }
